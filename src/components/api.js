6df2a8e0be26427540187ba2cf63fc7a0f58d597
constantine <ferrumresistance@gmail.com>
2024-01-19 18:22:35 +0200

    Point api to minikube

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "https://localhost:8765"
-export const SOCKET_URL = "wss://localhost:8765/budgets/";
+const API_URL = "http://api.localhost/"
+export const SOCKET_URL = "ws://api.localhost/budgets/";
 
 export const api = axios.create({
     baseURL: API_URL,
