d66714d783830f146139fedcfc2542c2f8707fb0
constantine <ferrumresistance@gmail.com>
2024-01-21 18:00:18 +0200

    update api for prod

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://api.localhost/"
-export const SOCKET_URL = "ws://api.localhost/budgets/";
+const API_URL = "https://api.mybudget.today/"
+export const SOCKET_URL = "wss://api.mybudget.today/budgets/";
 
 export const api = axios.create({
     baseURL: API_URL,
