cbd1bfba4dba50821508ca6bf9081902c4320269
constantine <ferrumresistance@gmail.com>
2024-11-16 19:58:51 +0200

    Change api address to home deployment

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "https://api.mybudget.today/"
-export const SOCKET_URL = "wss://api.mybudget.today/budgets/";
+const API_URL = "https://api.mybudget.home.arpa/"
+export const SOCKET_URL = "wss://api.mybudget.home.arpa/budgets/";
 
 export const api = axios.create({
     baseURL: API_URL,
