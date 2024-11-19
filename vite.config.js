04c385fa714a05214eec4e893d02e9de857bf79b
constantine <ferrumresistance@gmail.com>
2024-11-19 21:16:13 +0200

    Use https only on development

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,22 @@ import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    server: {
-        host: 'mybudget-test.home.arpa',
-        https: {
-            key: fs.readFileSync('./.certs/mybudget_Key.pem'),
-            cert: fs.readFileSync('./.certs/mybudget_Cert.pem'),
-        }
+export default defineConfig(({ mode }) => {
+    const isLocal = mode === 'development';
+
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        server: isLocal ? {
+            host: 'mybudget-test.home.arpa',
+            https: {
+                key: fs.readFileSync('./.certs/mybudget_Key.pem'),
+                cert: fs.readFileSync('./.certs/mybudget_Cert.pem'),
+            }
+        } : null,
     }
 })
