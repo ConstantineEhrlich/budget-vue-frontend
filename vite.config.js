f8ab7f563a5745e8377e1cbcef06809df11ace23
constantine <ferrumresistance@gmail.com>
2024-11-19 21:02:41 +0200

    Use local url for development + https

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import fs from 'fs'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -11,6 +13,13 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        host: 'mybudget-test.home.arpa',
+        https: {
+            key: fs.readFileSync('./.certs/mybudget_Key.pem'),
+            cert: fs.readFileSync('./.certs/mybudget_Cert.pem'),
         }
     }
 })
