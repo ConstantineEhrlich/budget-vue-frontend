be077b2d354adfb8ed104131572f5cc7b552dbdf
constantine <ferrumresistance@gmail.com>
2024-11-19 21:36:29 +0200

    Read api address from env var

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "https://api.mybudget.home.arpa/"
-export const SOCKET_URL = "wss://api.mybudget.home.arpa/budgets/";
+const API_URL = import.meta.env.VITE_API_URL;
+export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const api = axios.create({
     baseURL: API_URL,
